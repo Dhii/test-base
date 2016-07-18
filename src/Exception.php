a1310d5075f7cc8064b4517046e45e6f3e439c46
Anton Ukhanev <xedin.unknown@gmail.com>
2016-07-18 13:35:55 +0200

    Added `Exception` class.
    Tests included.

---
+++ src/Exception.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Dhii\Test;
+
+/**
+ * Base class for all Dhii test exceptions.
+ *
+ * @since [*next-version*]
+ */
+class Exception extends \PHPUnit_Framework_Exception
+{
+}
---
+++ test/unit/ExceptionTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Dhii\Test\Test;
+
+use Dhii\Test\Exception as TestSubject;
+
+/**
+ * Tests Exception.
+ *
+ * @since [*next-version*]
+ */
+class ExceptionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @since [*next-version*]
+     * @param string|null $message The message for the exception
+     * @return TestSubject The exception instance.
+     */
+    public function createException($message = null)
+    {
+        $subject = new TestSubject($message);
+        return $subject;
+    }
+
+    /**
+     * Tests that a vald PHPUnit exception can be created.
+     * @since [*next-version*]
+     */
+    public function testCanBeCreated()
+    {
+        $subject = $this->createException();
+        $this->assertInstanceOf(\PHPUnit_Framework_Exception::class, $subject, 'Exception must be a valid PHPUnit exception');
+    }
+
+    /**
+     * Tests that the exception can be thrown with the correct message.
+     * @since [*next-version*]
+     */
+    public function testCanBeThrown()
+    {
+        $token = '&^*&@e)SDIq!';
+        $subject = $this->createException($token);
+        $error = 'The exception must be throwable';
+
+        try {
+            throw $subject;
+        } catch (\Exception $ex) {
+            $this->assertContains($token, $ex->getMessage(), $error);
+            return;
+        }
+
+        $this->assertTrue(false, $error);
+    }
+}
