1e192db5de632c1f43c2dc7b79b1d0c4faa00f70
Anton Ukhanev <xedin.unknown@gmail.com>
2016-07-18 13:02:26 +0200

    Added `CanTestCanBeCreatedTrait`.
    Tests included.

---
+++ src/CanTestCanBeCreatedTrait.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dhii\Test;
+
+/**
+ * Functionality for testing whether the test subject can be created.
+ *
+ * @since [*next-version*]
+ */
+trait CanTestCanBeCreatedTrait
+{
+
+    public function testCanBeCreated()
+    {
+        $instance = $this->createInstance();
+        $className = $this->getClassName();
+        $this->assertClassType($instance, $className, sprintf('Object must be of type "%1$s"', $className));
+
+        if ($ancestorName = $this->getClassAncestor()) {
+            $this->assertInstanceOf($instance, $ancestorName, sprintf('Object must be an instance of "%1$s"', $ancestorName));
+        }
+    }
+
+    abstract public function createInstance();
+
+    abstract public function getClassName();
+    
+    abstract public function getClassAncestor();
+
+    abstract public function assertClassType($actual, $expected, $message = '');
+
+    abstract public function assertInstanceOf($actual, $expected, $message = '');
+}
---
+++ test/unit/CanTestCanBeCreatedTraitTest.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace Dhii\Test\Test;
+
+use Dhii\Test\CanTestCanBeCreatedTrait as TestSubject;
+use Dhii\Test\CanAssertAssertionFailureTrait as AssertionFailureTrait;
+use Dhii\Test\CanAssertAssertionSuccessTrait as AssertionSuccessTrait;
+
+/**
+ * Description of CanTestCanBeCreatedTraitTest
+ *
+ * @since [*next-version*]
+ */
+class CanTestCanBeCreatedTraitTest extends \PHPUnit_Framework_TestCase
+{
+    use AssertionFailureTrait;
+    use AssertionSuccessTrait;
+
+    /**
+     * Tests that the check for valid created class fails if not right class.
+     * @since [*next-version*]
+     */
+    public function testTestCanBeCreatedClassTypeFails()
+    {
+        $subjectClass = TestSubject::class;
+        $trait = $this->getMockForTrait($subjectClass);
+        $trait->method('createInstance')
+            ->willReturn('[instance of subject]');
+        $trait->method('getClassName')
+            ->willReturn('[class name]');
+        $trait->method('getClassAncestor')
+            ->willReturn('[ancestor name]');
+        $trait->method('assertClassType')
+            ->will($this->returnCallback(function($actual, $expected, $message = '') {
+                $this->assertTrue(false, $message);
+            }));
+
+        // Part of actual error message from the test subject
+        $classTypeError = 'Object must be of type';
+        // Due to the class type assertion failing, this should pass
+        $this->assertAssertionFailure(function() use ($trait) {
+                $trait->testCanBeCreated();
+        }, $classTypeError, 'Should not be able to create object due to type mismatch');
+    }
+
+    /**
+     * Tests that the check for valid created class fails if no right ancestor.
+     * @since [*next-version*]
+     */
+    public function testTestCanBeCreatedInstanceOfFails()
+    {
+        $subjectClass = TestSubject::class;
+        $trait = $this->getMockForTrait($subjectClass);
+        $trait->method('createInstance')
+            ->willReturn('[instance of subject]');
+        $trait->method('getClassName')
+            ->willReturn('[class name]');
+        $trait->method('getClassAncestor')
+            ->willReturn('[ancestor name]');
+        $trait->method('assertClassType')
+            ->will($this->returnCallback(function($actual, $expected, $message = '') {
+                $this->assertTrue(true, 'This passes in this example');
+            }));
+
+        $trait->method('assertInstanceOf')
+            ->will($this->returnCallback(function($actual, $expected, $message = '') {
+                $this->assertTrue(false, $message);
+            }));
+
+        // Part of actual error message from the test subject
+        $classTypeError = 'Object must be an instance of';
+        // Due to the class type assertion failing, this should pass
+        $this->assertAssertionFailure(function() use ($trait) {
+                $trait->testCanBeCreated();
+        }, $classTypeError, 'Should not be able to create object due to type mismatch');
+    }
+
+    /**
+     * Tests that the check for valid created class succeeds under the right conditions.
+     * @since [*next-version*]
+     */
+    public function testTestCanBeCreatedSuccess()
+    {
+        $subjectClass = TestSubject::class;
+        $trait = $this->getMockForTrait($subjectClass);
+        $trait->method('createInstance')
+            ->willReturn('[instance of subject]');
+        $trait->method('getClassName')
+            ->willReturn('[class name]');
+        $trait->method('getClassAncestor')
+            ->willReturn('[ancestor name]');
+        $trait->method('assertClassType')
+            ->will($this->returnCallback(function($actual, $expected, $message = '') {
+                $this->assertTrue(true, 'This passes in this example');
+            }));
+
+        $trait->method('assertInstanceOf')
+            ->will($this->returnCallback(function($actual, $expected, $message = '') {
+                $this->assertTrue(true, 'Also passes');
+            }));
+
+        // Due to the class type assertion succeeding, this should pass
+        $this->assertAssertionSuccess(function() use ($trait) {
+                $trait->testCanBeCreated();
+        }, 'Should be able to create object');
+    }
+}
