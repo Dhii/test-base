d4937bed378f7c59cfaa5bbc4f69bc0042667f4a
Anton Ukhanev <xedin.unknown@gmail.com>
2016-08-17 13:23:04 +0200

    Added docs to `Assertion\TypeTrait#assertClassType()`

--- src/Assertion/TypeTrait.php
+++ src/Assertion/TypeTrait.php
@@ -13,6 +13,15 @@ trait TypeTrait
 {
     use ThatTrait;
 
+    /**
+     * Asserts that the given object is exactly of a specified class type.
+     *
+     * @since [*next-version*]
+     *
+     * @param string $expected The fully qualified name of the expected type.
+     * @param object $actual   The object to test.
+     * @param string $message  The message to output if the test fails.
+     */
     public function assertClassType($expected, $actual, $message = '')
     {
         $constraint = new Constraint\Type($expected);
